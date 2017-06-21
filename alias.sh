a5f9e9a6c65fdb05c42fa34a793c2b7ef6ce10c9
jcomeme <hara@sunyou.co.jp>
2017-06-21 14:02:15 +0900

    d

--- alias.sh
+++ alias.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim/colors ~/.vim/colors
 ln -sf ~/dotfiles/.vim/snippets ~/.vim/snippets
+ln -sf ~/dotfiles/.vim/dein/repos/github.com/Shougo/dein.vim ~/.vim/dein/repos/github.com/Shougo/dein.vim
+
